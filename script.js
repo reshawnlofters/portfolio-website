54b0970eca4cb4a7dcc9f99f8ff31a7545d649f6
Reshawn Lofters <reshawnalofters@gmail.com>
2023-05-02 16:11:49 -0400

    feat: allow the user to open and close toggle menu

---
+++ script.js
@@ -0,0 +1,14 @@
+// variables
+var navMenu = document.getElementById("navMenu");
+
+// function to show toggle menu
+function showMenu()
+{
+    navMenu.style.right = "0";
+}
+
+// function to hide toggle menu
+function hideMenu()
+{
+    navMenu.style.right = "-200px";
+}
