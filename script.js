4da12151cf4c8f6d3bb0afc77ecd2ccd02ceca4c
Reshawn Lofters <reshawnalofters@gmail.com>
2023-05-03 16:32:48 -0400

    feat: track contact form textarea character inputs
    Track the number of characters a user enters into the contact form
    textarea.
    
    Display a red border on the textarea when the maximum character length
    is reached.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // variables
-var navMenu = document.getElementById("navMenu");
+let navMenu = document.getElementById("navMenu"),
+    inputContainer = document.querySelector(".form-textarea-container"),
+    textarea = inputContainer.querySelector("textarea"),
+    currentNumber = inputContainer.querySelector(".current-number");
 
 // function to show toggle menu
 function showMenu()
@@ -12,3 +15,18 @@ function hideMenu()
 {
     navMenu.style.right = "-200px";
 }
+
+// function to track the contact form textarea characters
+textarea.addEventListener("keyup", () =>
+{
+    let characterLength = textarea.value.length;
+        currentNumber.innerHTML = characterLength;
+
+    characterLength > 0 
+    ? inputContainer.classList.add("active") 
+    : inputContainer.classList.remove("active");
+
+    characterLength > 1000 
+    ? inputContainer.classList.add("error") 
+    : inputContainer.classList.remove("error");
+})
