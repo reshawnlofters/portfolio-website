03bb267858a270b0e69fe7122f3f4d7439d0368d
Reshawn Lofters <reshawnalofters@gmail.com>
2023-05-03 18:40:42 -0400

    chore: create a folder to separate page scripts

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,41 +1,26 @@
 // variables
-let navMenu = document.getElementById('navMenu'),
-    inputContainer = document.querySelector('.form-textarea-container'),
+let inputContainer = document.querySelector('.form-textarea-container'),
     textarea = inputContainer.querySelector('textarea'),
     currentNumber = inputContainer.querySelector('.current-number');
-    formSubmitMessage
+    const contactForm = document.getElementById('contactForm'),
+    contactMessage = document.getElementById('formSubmitMessage')
 
-// function to show toggle menu
-function showMenu()
-{
-    navMenu.style.right = '0';
-}
-
-// function to hide toggle menu
-function hideMenu()
-{
-    navMenu.style.right = '-200px';
-}
-
-// function to track the contact form textarea characters
+// function to track the inputted contact form textarea characters
 textarea.addEventListener('keyup', () =>
 {
     let characterLength = textarea.value.length;
         currentNumber.innerHTML = characterLength;
 
     characterLength > 0 
-    ? inputContainer.classList.add('active') 
-    : inputContainer.classList.remove('active');
+        ? inputContainer.classList.add('active') 
+        : inputContainer.classList.remove('active');
 
     characterLength > 1000 
-    ? inputContainer.classList.add('error') 
-    : inputContainer.classList.remove('error');
+        ? inputContainer.classList.add('error') 
+        : inputContainer.classList.remove('error');
 })
 
-// function to send an email when contact form is submitted
-const contactForm = document.getElementById('contactForm'),
-    contactMessage = document.getElementById('formSubmitMessage')
-
+// function to send an email when the contact form is submitted
 const sendEmail = (e) =>
 {
     e.preventDefault()
@@ -51,6 +36,7 @@ const sendEmail = (e) =>
             setTimeout(() =>
             {
                 contactMessage.textContent = ''
+                
             }, 5000)
 
             // clear input fields
