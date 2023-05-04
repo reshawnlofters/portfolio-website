df75f0e7b2ab0483f06867f0e58eb5b7c1570592
Reshawn Lofters <reshawnalofters@gmail.com>
2023-05-04 12:15:03 -0400

    chore: combine webpage javascript in a single file

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 // variables
-let inputContainer = document.querySelector('.form-textarea-container'),
+let navMenu = document.getElementById('navMenu');
+    inputContainer = document.querySelector('.form-textarea-container'),
     textarea = inputContainer.querySelector('textarea'),
     currentNumber = inputContainer.querySelector('.current-number');
     const contactForm = document.getElementById('contactForm'),
     contactMessage = document.getElementById('formSubmitMessage')
 
-// function to track the inputted contact form textarea characters
+// function to show toggle menu
+function showMenu()
+{
+    navMenu.style.right = '0';
+}
+
+// function to hide toggle menu
+function hideMenu()
+{
+    navMenu.style.right = '-200px';
+}
+
+// track the inputted contact form textarea characters
 textarea.addEventListener('keyup', () =>
 {
-    let characterLength = textarea.value.length;
-        currentNumber.innerHTML = characterLength;
+let characterLength = textarea.value.length;
+    currentNumber.innerHTML = characterLength;
 
-    characterLength > 0 
-        ? inputContainer.classList.add('active') 
-        : inputContainer.classList.remove('active');
+characterLength > 0 
+    ? inputContainer.classList.add('active') 
+    : inputContainer.classList.remove('active');
 
-    characterLength > 1000 
-        ? inputContainer.classList.add('error') 
-        : inputContainer.classList.remove('error');
+characterLength > 1000 
+    ? inputContainer.classList.add('error') 
+    : inputContainer.classList.remove('error');
 })
 
-// function to send an email when the contact form is submitted
+// send an email when the contact form is submitted
 const sendEmail = (e) =>
 {
     e.preventDefault()
@@ -29,7 +42,7 @@ const sendEmail = (e) =>
     emailjs.sendForm('service_3fgmcge', 'template_a6mq49i', '#contactForm', '9-ljn6pGNdbjVUgzh')
         .then(() =>
         {
-            // display sent message
+            // display success message
             contactMessage.textContent = 'Message sent successfully'
 
             // remove message after five seconds
